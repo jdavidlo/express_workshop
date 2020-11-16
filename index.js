9cbd0c97b6cf3b6fe79137b4cca99da148f05978
jdavidlo <jdibicho@gmail.com>
2020-11-16 13:34:09 -0600

    act 13 implementando jwt

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+// Dependencies
 const morgan = require('morgan');
 const express = require('express');
 const app = express();
+// Routers
 const pokemon = require('./routes/pokemon');
 const user = require('./routes/user');
+// Middleware
+const auth = require('./middleware/auth');
+const notFound = require('./middleware/notFound');
+const index = require('./middleware/index')
 
 app.use(morgan('dev'));
 app.use(express.json())
@@ -17,17 +23,15 @@ app.use(express.urlencoded({extended: true}))
 // PUT - modificar 
 // DELETE - borrar
 
-app.get("/", (req, res, next) =>{
+app.get("/", index)
 
-    return res.status(200).json({code: 1, message: "Bienvenido al Pokedex"})
-})
-
-app.use("/pokemon", pokemon)
 app.use("/user", user)
 
-app.use((req, res, next) => {
-    return res.status(404).json({code: 404, message: "URL no encontrada"})
-})
+app.use(auth)
+
+app.use("/pokemon", pokemon)
+
+app.use(notFound)
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running');
